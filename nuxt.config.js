a4249493ccc0a6d6bd65ec5e993f23e735ea1835
boubekeurbenzaid <boubekeur_benzaid@marketpay.eu>
2021-12-01 09:34:35 +0100

    Add dotenv & nuxt-mail (nodemailer) modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,62 @@
+require('dotenv').config()
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'api-sendgrid',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/dotenv',
+    'nuxt-mail',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
+  // Dotenv module configuration: https://www.npmjs.com/package/@nuxtjs/dotenv
+  dotenv: {},
+
+  // Nuxt-mail module configuration: https://www.npmjs.com/package/nuxt-mail
+  mail: {
+    message: {
+      to: process.env.MAILTO,
+    },
+    smtp: {
+      host: process.env.HOST,
+      port: process.env.PORT,
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    },
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
